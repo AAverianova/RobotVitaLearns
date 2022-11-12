aa912383a29c8d766cf6c92bc621e4604569aebe
A. Averianova <>
2022-11-12 05:23:38 +0000

    3

--- classes.py
+++ classes.py
@@ -3,22 +3,35 @@
 """
 printComments = True #Для вывода всех комментариев
 
-from abc import ABC, abstractmethod
+#from abc import ABC, abstractmethod
+class Thing:
+  def __init__(self):
+    pass
+    
 
-
-class Robot:
+class Robot (Thing):
     def __init__(self, number: str, name:str)->None:
       self.__number = number
       self.__name = name # приватные поля
       if printComments:
-        print("      **Объект создан.")
+        print("____________Объект создан.********************************")
     def print(self):
       print("Robot", self.__name, "has num = ", self.__number)
       if printComments:
-        print("      **Объект выведен на экран.")
+        print("____________Объект выведен на экран.**********************")
 
     def setName(self, newName):
       self.__name=newName
       if printComments:
-        print("      **Объекту", self.__number, "сменили имя на", newName)
+        print("____________Объекту", self.__number, "сменили имя на", newName,".*********")
       
+class Decorator(Thing):
+  def __init__(self,who):
+    self.__who=who
+
+class Building (Decorator):
+  def __init__(self,who):
+     super().__init__()
+  def createHouse(self):
+    
+  
