90a2bdc87f800d070c7e6817fdc62fdbdd5d7605
A. Averianova <>
2022-11-12 05:12:02 +0000

    2

--- classes.py
+++ classes.py
@@ -1,13 +1,24 @@
 """
 Классы расположены здесь
 """
+printComments = True #Для вывода всех комментариев
 
 from abc import ABC, abstractmethod
 
 
 class Robot:
     def __init__(self, number: str, name:str)->None:
-        self.number = number
-        self.name = name
+      self.__number = number
+      self.__name = name # приватные поля
+      if printComments:
+        print("      **Объект создан.")
     def print(self):
-      print("Robot ", self.name, "has num = ", self.number)
+      print("Robot", self.__name, "has num = ", self.__number)
+      if printComments:
+        print("      **Объект выведен на экран.")
+
+    def setName(self, newName):
+      self.__name=newName
+      if printComments:
+        print("      **Объекту", self.__number, "сменили имя на", newName)
+      
