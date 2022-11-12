284be41cc11456e35bcb73820f3061cb9a03e133
A. Averianova <>
2022-11-12 05:52:46 +0000

    FirstLearn

--- classes.py
+++ classes.py
@@ -4,12 +4,17 @@ class Decorator():
   def __init__(self,who):
     self.__who=who
   def getName(self):
-    return self.getName()
+    return self.__who.getName()
   
 
-class Building (Decorator):
+class FirstLearn(Decorator):
   def __init__(self,who):
-     super().__init__()
+     super().__init__(who)
+     print("++++объект", self.getName(), "научился строить дом методом createHouse")
+     print("++++объект", self.getName(), "научился строить сарай методом createBarn")
   def createHouse(self):
-     print("____________Объект", "построил дом.*****************")
-  
+     print("____________Объект",self.getName(), "построил дом.*****************")
+  def createBarn(self):
+     print("____________Объект",self.getName(), "построил сарай.*****************")
+  def getName(self):
+    return super().getName()
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from classes import Robot
+from classes import *
 print("Создаём робота...\n")
 
 # Создание робота 
 robot = Robot("АА001221-56", "В") 
 robot.print()
-
+#Первичное обучение
+decor = FirstLearn(robot)
+#Переименование робота
+robot.setName("Вита")
+robot.print()
+# Проверка что  можно построить
+decor.createHouse()
+decor.createBarn()
 #Переименование робота
 robot.setName("Вита")
 robot.print()
